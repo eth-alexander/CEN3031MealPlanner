d35fc8fc5a237317a70c77a4e13ac064475dfc7a
Lia Ranon <lia.ranon@gmail.com>
2024-11-24 17:47:22 -0500

    basic create account

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,14 +2,15 @@ import './Home.css';
 import Login from "./Login/Login";
 import useToken from './Login/useToken';
 
-function Home() {
+function Home() { 
 
   const {token, setToken} = useToken();
+  console.log(token);
   if(!token) {
     return (
       <>
         <h1 className="Home">Home</h1>
-        <Login setToken={setToken} />
+        <Login setToken={setToken} /> 
       </>
     )
     
