3e2195cfcec2811d636ce219a8defe3f10ebee2b
Lia Ranon <lia.ranon@gmail.com>
2024-12-03 17:55:41 -0500

    update home

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,17 +1,28 @@
 import './Home.css';
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 
 function Home() {
 
   const {state} = useLocation();
   const { user } = state; // Read values passed on state
  
+
   return (
     <>
       <h1 className="Home">Home</h1>
       Welcome {user}!
+      <br></br>
+      <br></br>
+      Browse <a> <Link to="/recipes">Recipes</Link> </a>
+      <br></br>
+      <br></br>
+      Look at saved recipes in your  <a><Link to="/dashboad">Dashboard</Link></a> 
+      
     </>
   );
+  
 }
 
 export default Home;
+
+
