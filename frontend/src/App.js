393dd7c12d4af343598b163011bdbda89f76726d
Jake Lang <114430058+JangleDev@users.noreply.github.com>
2024-12-04 13:00:15 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,40 @@
-/*import logo from './logo.svg';
-import './App.css';*/
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Recipes from "./pages/Recipes";
 import Products from "./pages/Products";
-import Dashboard from "./pages/private/Dashboard"
-import Login from "./pages/Login/Login";
-import useToken from './pages/Login/useToken';
+import Dashboard from "./pages/private/Dashboard";
+import Login from "./pages/Login/Login";  // Assuming Login is defined
 
 function App() {
-  const {token, setToken} = useToken();
-  if(!token) {
-    return <Login setToken={setToken} />
-  }
+  const [token, setToken] = useState(localStorage.getItem('token'));
+  
 
   return (
-    <div>
-      <BrowserRouter>
-      <Routes> 
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="recipes" element={<Recipes />} />
-          <Route path="products" element={<Products />} />
-          <Route path="dashboard" element={<Dashboard />} />  
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Public route for Login */}
+          <Route path="/login" element={<Login setToken={setToken} />} />
+
+          {/* Protected route: If the user is authenticated, navigate to Home, else login */}
+          <Route path="/" element={token ? <Navigate to="/home" /> : <Navigate to="/login" />} />
+
+          {/* Protected routes wrapped with Layout */}
+          <Route element={token ? <Layout /> : <Navigate to="/login" />}>
+            <Route path="/home" element={<Home />} />
+            <Route path="/recipes" element={<Recipes />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
