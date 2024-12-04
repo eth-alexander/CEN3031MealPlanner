3687e641bd236021f263b0adbc35b4c13cff0bf7
Rushilkothur <rushil.kothur@ufl.edu>
2024-12-04 00:15:42 -0500

    fix user model-saved_recipe variable and added delete endpoint to test out

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
     username: {type: String, required: true},
     password: { type: String, required: true },
-    saved_recipes: { type: Array, required: true},
+    saved_recipes: { type: [mongoose.Schema.Types.ObjectId], ref: 'Meal', default: [] },
 })
 
 const userModel = mongoose.model("User", userSchema)
