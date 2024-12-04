65b41eee0bd6ea74f75db0d171ca888b2d5b4679
Jake Lang <114430058+JangleDev@users.noreply.github.com>
2024-12-04 13:00:47 -0500

    Update Layout.js

--- frontend/src/pages/Layout.js
+++ frontend/src/pages/Layout.js
@@ -1,14 +1,27 @@
 import './Layout.css';
 import { Outlet, Link } from "react-router-dom";
+import useToken from './Login/useToken';
+
+
 
 function Layout() {
+
+  const { clearToken } = useToken();  // Get the clearToken function
+  
+ 
+  const handleLogout = () => {
+    clearToken();  // Clear the token
+    localStorage.clear();
+    window.location.href = '/login';  // Redirect to login page after logout
+  };
+
   return (
     <>
       <nav> 
            <h1 className='Layout'> <a> <Link to="/">Home</Link> </a>
            <a> <Link to="/recipes">Recipes</Link> </a>
-           <a> <Link to="/products">Products</Link> </a> 
            <a><Link to="/dashboard">Dashboard</Link></a> </h1>
+           <button onClick={handleLogout} >Logout</button> {/* Logout button */}
       </nav>
 
       <Outlet />
